4906c4986bfbc1014dfb06bcc73a1b69f30299c1
Tamir Cohen <tamircohen14@gmail.com>
2024-01-21 22:44:33 +0200

    fixing to device stuff

--- final_exercise/solution/trainer.py
+++ final_exercise/solution/trainer.py
@@ -60,7 +60,9 @@ class Trainer:
         print_every = int(len(train_dataloader) / 10)
 
         for batch_idx, (inputs, targets) in enumerate(train_dataloader):
-            accuracies = self.model(inputs.to(device))
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            accuracies = self.model(inputs)
             prediction = torch.argmax(accuracies, dim=1)
             loss = self.criterion(accuracies, targets)
             loss.backward()
@@ -104,7 +106,9 @@ class Trainer:
         print_every = max(int(len(dataloader) / 10), 1)
 
         for batch_idx, (inputs, targets) in enumerate(dataloader):
-            accuracies = self.model(inputs.to(device))
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            accuracies = self.model(inputs)
             prediction = torch.argmax(accuracies, dim=1)
             loss = self.criterion(accuracies, targets)
             total_loss += loss.item()
